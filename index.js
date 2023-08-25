5e0ce36effed2a3cc4cccc8ba47393921fc9b9ce
IntegerAlex <inquiry.akshatkotpalliwar@gmail.com>
2023-08-26 01:21:01 +0530

    update

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const validUrl = require('valid-url'); // External library to validate URLs
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,35 +12,30 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Simulated data store for storing short URLs
 const shortUrls = {};
+let shortUrlCounter = 1;
 
 app.post('/api/shorturl', (req, res) => {
   const originalUrl = req.body.url;
 
-  // Simulating short URL creation
-  const shortUrl = 'https://short.url/' + Math.floor(Math.random() * 10000);
+  // Validate the URL
+  if (!validUrl.isWebUri(originalUrl)) {
+    console.error(`Invalid URL: ${originalUrl}`);
+    return res.status(400).json({ error: 'invalid url' });
+  }
 
-  // Store short URL in the data store
+  const shortUrl = shortUrlCounter++;
   shortUrls[shortUrl] = originalUrl;
 
   res.json({ original_url: originalUrl, short_url: shortUrl });
 });
 
-// Redirect from short URL to original URL
+
 app.get('/api/shorturl/:short', (req, res) => {
-  const shortUrl = 'https://short.url/' + req.params.short;
+  const shortUrl = parseInt(req.params.short);
   const originalUrl = shortUrls[shortUrl];
 
   if (originalUrl) {
